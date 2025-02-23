3c2462f098eeb8ea57b52e5191c0f59671319062
Junseo Kim <semiwest2@gmail.com>
2025-02-23 17:04:36 +0900

    feat: 관리자 페이지 스타일 컴포넌트 추가

---
+++ frontend/src/pages/AdminPage/AdminPage.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const AdminPageContainer = styled.div`
+    display: flex;
+    gap: 34.5px;
+    margin-top: 98.5px;
+`;
+
+export const Content = styled.main`
+    width: 100%;
+    border: 1px solid #dcdcdc;
+    border-radius: 18px;
+    padding: 62px 58px;
+`;
