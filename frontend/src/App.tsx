d43cc67a9d90b8ee6eb151e985923346fda2f9e6
Junseo Kim <semiwest2@gmail.com>
2025-02-23 15:01:20 +0900

    feat: 관리자 페이지 라우트 추가
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyles from '@/styles/Global.styles';
 import MainPage from '@/pages/MainPage/MainPage';
 import ClubDetailPage from '@/pages/ClubDetailPage/ClubDetailPage';
+import AdminPage from './pages/AdminPage/AdminPage';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<MainPage />} />
           <Route path='/ClubDetail' element={<ClubDetailPage />} />
+          <Route path='/admin' element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
