b3e0800eb15b81bfcf5c8a0c891e4654b9b871df
FhRh <yiyop@naver.com>
2025-02-24 00:27:43 +0900

    fix : 검색결과 응급처치
    - 태그 및 information에 관한 정보가 나오지 않아 응급처치 합니다.

--- backend/src/main/java/moadong/club/repository/ClubTagRepository.java
+++ backend/src/main/java/moadong/club/repository/ClubTagRepository.java
@@ -1,14 +1,16 @@
 package moadong.club.repository;
 
+import java.util.List;
+import java.util.Optional;
 import moadong.club.entity.ClubTag;
 import moadong.club.payload.dto.ClubTagProjection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ClubTagRepository extends MongoRepository<ClubTag, String> {
+
     Optional<List<ClubTagProjection>> findAllByClubId(String clubId);
+
+    List<ClubTag> findClubTagsByClubId(String clubId);
 
     void deleteAllByClubId(String clubId);
 }
--- backend/src/main/java/moadong/club/service/ClubSearchService.java
+++ backend/src/main/java/moadong/club/service/ClubSearchService.java
@@ -1,32 +1,60 @@
 package moadong.club.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import moadong.club.entity.ClubInformation;
+import moadong.club.entity.ClubTag;
 import moadong.club.payload.dto.ClubSearchResult;
 import moadong.club.payload.response.ClubSearchResponse;
+import moadong.club.repository.ClubInformationRepository;
 import moadong.club.repository.ClubSearchRepository;
+import moadong.club.repository.ClubTagRepository;
+import moadong.global.exception.ErrorCode;
+import moadong.global.exception.RestApiException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ClubSearchService {
+
     private final ClubSearchRepository clubSearchRepository;
+    private final ClubInformationRepository clubInformationRepository;
+    private final ClubTagRepository clubTagRepository;
 
     public ClubSearchResponse searchClubsByKeyword(String keyword,
-                                                   String recruitmentStatus,
-                                                   String division,
-                                                   String classification
+        String recruitmentStatus,
+        String division,
+        String classification
     ) {
         List<ClubSearchResult> clubSearchResults = clubSearchRepository.searchResult(
-                keyword,
-                recruitmentStatus,
-                division,
-                classification
+            keyword,
+            recruitmentStatus,
+            division,
+            classification
         );
 
+        List<ClubSearchResult> result = assignInformation(clubSearchResults);
+
         return ClubSearchResponse.builder()
-                .clubs(clubSearchResults)
-                .build();
+            .clubs(result)
+            .build();
+    }
+
+    private List<ClubSearchResult> assignInformation(List<ClubSearchResult> clubSearchResults) {
+        List<ClubSearchResult> list = new ArrayList<>();
+        for (ClubSearchResult clubSearchResult : clubSearchResults) {
+            ClubInformation clubInformation = clubInformationRepository.findByClubId(
+                    clubSearchResult.id())
+                .orElseThrow(() -> new RestApiException(ErrorCode.CLUB_INFORMATION_NOT_FOUND));
+
+            List<ClubTag> tags = clubTagRepository.findClubTagsByClubId(clubSearchResult.id());
+            List<String> tagNames = tags.stream()
+                .map(ClubTag::getTag)
+                .toList();
+
+            list.add(ClubSearchResult.of(clubSearchResult, clubInformation, tagNames));
+        }
+        return list;
     }
 }
