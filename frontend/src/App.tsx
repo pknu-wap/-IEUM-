87755408d8088bda2313d00247a683214ff0ba96
Junseo Kim <semiwest2@gmail.com>
2025-02-23 17:04:59 +0900

    feat: 관리자 페이지에 하위 경로 추가 및 리디렉션 설정
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyles from '@/styles/Global.styles';
 import MainPage from '@/pages/MainPage/MainPage';
 import ClubDetailPage from '@/pages/ClubDetailPage/ClubDetailPage';
 import AdminPage from './pages/AdminPage/AdminPage';
+import ClubInfoEditTab from '@/pages/AdminPage/tabs/ClubInfoEditTab/ClubInfoEditTab';
+import RecruitEditTab from './pages/AdminPage/tabs/RecruitEditTab/RecruitEditTab';
+import AccountEditTab from './pages/AdminPage/tabs/AccountEditTab/AccountEditTab';
 
 const queryClient = new QueryClient();
 
@@ -16,7 +18,13 @@ const App = () => {
         <Routes>
           <Route path='/' element={<MainPage />} />
           <Route path='/ClubDetail' element={<ClubDetailPage />} />
-          <Route path='/admin' element={<AdminPage />} />
+          <Route path='/admin' element={<AdminPage />}>
+            <Route index element={<Navigate to="club-info" replace />} />
+            <Route path="club-info" element={<ClubInfoEditTab />} />
+            <Route path="recruit-edit" element={<RecruitEditTab />} />
+            <Route path="account-edit" element={<AccountEditTab />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
