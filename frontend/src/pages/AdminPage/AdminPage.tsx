ab73e3720eefe937525d74b5ec214f05f8288076
Junseo Kim <semiwest2@gmail.com>
2025-02-23 17:04:42 +0900

    feat: 관리자 페이지에 Outlet 추가 및 레이아웃 개선

--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import Header from '@/components/common/Header/Header';
 import { PageContainer } from '@/styles/PageContainer.styles';
 import SideBar from '@/pages/AdminPage/components/SideBar/SideBar';
+import { Outlet } from 'react-router-dom';
+import * as Styled from './AdminPage.styles';
 
 const AdminPage = () => {
 
     return (
         <>
+            <Header />
             <PageContainer>
-                <SideBar />
+                <Styled.AdminPageContainer>
+                    <SideBar />
+                    <Styled.Content>
+                        <Outlet />
+                    </Styled.Content>
+                </Styled.AdminPageContainer>
             </PageContainer>
         </>
     );
