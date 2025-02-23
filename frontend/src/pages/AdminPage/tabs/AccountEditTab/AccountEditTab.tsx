a5b68af397395611a53be038bda1ff6f5015ca8a
Junseo Kim <semiwest2@gmail.com>
2025-02-23 17:00:42 +0900

    feat: 관리자 페이지에 계정 정보 편집 탭 컴포넌트 추가
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

---
+++ frontend/src/pages/AdminPage/tabs/AccountEditTab/AccountEditTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccountEditTab = () => {
+    return (
+        <div>
+            <h1>Account Edit Tab</h1>
+        </div>
+    );
+};
+
+export default AccountEditTab;
