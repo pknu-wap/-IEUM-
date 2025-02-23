b6863ccd7f0311583e6cfb2cc556ba89722b7b80
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-23 16:48:35 +0900

    feat: 메인페이지 헤더 모바일 컴포넌트 추가

---
+++ frontend/src/pages/MainPage/components/MainMobileHeader/MainMobileHeader.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as Styled from './MainMobileHeader.styles';
+import MainIcon from '@/assets/images/mainIcon.png';
+import SearchIcon from '@/assets/images/searchIcon.png';
+import { useNavigate } from 'react-router-dom';
+
+const MainMobileHeader = () => {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <Styled.MobileHeaderContainer>
+      <Styled.MobileHeaderWrapper>
+        <Styled.MobileMainIcon>
+          <img src={MainIcon} onClick={handleHomeClick} />
+        </Styled.MobileMainIcon>
+        <Styled.MobileSubContainer>
+          <Styled.MobileSearchIcon>
+            <img src={SearchIcon} />
+          </Styled.MobileSearchIcon>
+          <Styled.MobileMenu>
+            <img src={SearchIcon} />
+          </Styled.MobileMenu>
+        </Styled.MobileSubContainer>
+      </Styled.MobileHeaderWrapper>
+    </Styled.MobileHeaderContainer>
+  );
+};
+export default MainMobileHeader;
