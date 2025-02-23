18df8770e0614e3309cad2bf5513e2f8293032b7
FhRh <yiyop@naver.com>
2025-02-23 19:28:55 +0900

    fix : corsConfiguration 명시

--- backend/src/main/java/moadong/global/config/SecurityConfig.java
+++ backend/src/main/java/moadong/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import moadong.global.util.JwtAuthenticationFilter;
 import moadong.global.util.JwtProvider;
 import moadong.user.service.CustomUserDetailService;
@@ -23,6 +24,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +36,8 @@ public class SecurityConfig {
     private final CustomUserDetailService userDetailsService;
     @Value("${spring.cloud.gcp.credentials.location}")
     private String credentialsLocation;
+    @Value("${frontend.domain}")
+    private String frontendDomain;
 
     public SecurityConfig(JwtProvider jwtProvider, CustomUserDetailService userDetailsService) {
         this.jwtProvider = jwtProvider;
@@ -43,6 +48,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) //cors 설정
             .authorizeHttpRequests(authorize -> authorize
                 .anyRequest().permitAll() // 모든 요청에 대해 인증 해제
             );
@@ -60,6 +66,20 @@ public class SecurityConfig {
                 .setCredentials(GoogleCredentials.fromStream(keyFile))
                 .build()
                 .getService();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList(frontendDomain, "http://localhost:*", "https://deploy-preview-76--moadong.netlify.app/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
