1c3f95e7da744ca6a0ff160d8daaa604095244a5
Junseo Kim <semiwest2@gmail.com>
2025-02-23 17:01:12 +0900

    refactor: 사이드바 컴포넌트를 div에서 aside로 변경

--- frontend/src/pages/AdminPage/components/SideBar/SideBar.styles.ts
+++ frontend/src/pages/AdminPage/components/SideBar/SideBar.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const SidebarWrapper = styled.div`
+export const SidebarWrapper = styled.aside`
     display: flex;
     flex-direction: column;
     align-items: left;
