94def107514c07d1b2505ac12c6aa8459818827c
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-23 16:42:42 +0900

    fix: header 불필요한 스타일 제거

--- frontend/src/components/common/Header/Header.styles.ts
+++ frontend/src/components/common/Header/Header.styles.ts
@@ -13,11 +13,7 @@ export const HeaderStyles = styled.header`
   background-color: white;
 
   @media (max-width: 500px) {
-    padding: 0 20px;
-  }
-
-  @media (max-width: 375px) {
-    padding: 0 10px;
+    display: none;
   }
 `;
 
@@ -40,10 +36,6 @@ export const LogoButtonStyles = styled.button`
   height: 43px;
   background-color: transparent;
   border: none;
-  font-weight: 400;
-  font-size: 26px;
-  line-height: 40px;
-  letter-spacing: -0.03em;
   color: #000000;
   cursor: pointer;
 
